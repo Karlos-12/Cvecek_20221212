1fb695d685eade904514df634e9126aa37ce0b79
Karel Cveček <Cvecek.Karel@stud.vassboskovice.cz>
2022-12-12 12:22:39 +0100

    Přidejte soubory projektu.

---
+++ Cvecek_20221212/Solider.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cvecek_20221212
+{
+    internal class Solider
+    {
+        public string name { get; set; }
+        public int hp { get; set; }
+        public int MaxHp { get; internal set; }
+        public int damage { get; set; }
+        public int shield { get; set; }
+        public int Maxshield { get; internal set; }
+        public int levl { get; set; }
+
+        public Solider()
+        {
+
+        }
+
+        public void Atack(Solider enemy)
+        {
+            enemy.hp -= damage - enemy.shield;
+        }
+
+        private int damageget()
+        {
+            Random random = new Random();
+            random.Next(damage - 5, damage + 5);
+        }
+
+        public void Heal()
+        {
+
+        }
+
+        public void levelup()
+        {
+            levl++;
+            MaxHp += 10;
+            Maxshield += 5; 
+        }
+
+    }
+}
